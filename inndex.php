da7898ecee8570b6e0102d3125d4c798218f37bc
luccaseler <126515107+luccaseler@users.noreply.github.com>
2023-02-27 16:45:25 -0300

    Add files via upload

---
+++ inndex.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <H1>
+        <?php echo "ola mundao"
+        ?>
+    </H1>
+    <h2> 12 - 3 = <?php echo (12 - 3) ?></h2>
+    <h2> 12 + 15 = <?php echo (12 + 15) ?></h2>
+    <h2> 12 * 171 = <?php echo (12 * 171) ?></h2>
+    <h2> 3/3 = <?php echo (3 / 3) ?></h2>
+ 
+    
+</body>
+</html>
